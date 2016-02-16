39c4be6413eec8e3729b8444927bb216c42d6fb5
lukedottec <lukedottec@gmail.com>
2016-02-16 15:37:43 -0800

    Here we fucking go.

---
+++ app/build/generated/source/buildConfig/androidTest/debug/lar/wsu/edu/airpact_fire/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package lar.wsu.edu.airpact_fire.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "lar.wsu.edu.airpact_fire.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
