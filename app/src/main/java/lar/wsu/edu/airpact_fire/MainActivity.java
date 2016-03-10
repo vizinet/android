fd2163cc97e15f5cfef7539f5b99b8b8c8871ebe
Luke Weber <lukedottec@gmail.com>
2016-03-10 06:43:16 -0800

    Added ability to have dots, changed formatting, and some other small features.

--- app/src/main/java/lar/wsu/edu/airpact_fire/MainActivity.java
+++ app/src/main/java/lar/wsu/edu/airpact_fire/MainActivity.java
@@ -2,6 +2,9 @@ package lar.wsu.edu.airpact_fire;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
@@ -16,6 +19,7 @@ import android.view.View;
 import android.widget.AbsoluteLayout;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,6 +33,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -40,19 +46,18 @@ import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/* @Todo ---------------------------------------
- *      - Save data as JSON (let web team know structure)
- *      - Send data to server (web team )
-  ---------------------------------------------*/
-
 // Acting as the ImageCaptureActivity
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
 
-    ImageView mImageView, mBlackDotView, mWhiteDotView;
+    // false = black, true = white
+    boolean colorMode;
+
+    ImageView mImageView, mBlackDotView, mWhiteDotView, mHighColorPatchView, mLowColorPatchView;
+    ImageButton mDotModeImageButton;
     Button mCameraButton, mUploadButton;
-    EditText mEditText;
+    EditText mEditText, mUserEditText;
     TextView mDebugText;
     String mCurrentPhotoPath;
     // [Not real server URL]
@@ -67,14 +72,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Set action bar stuff
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.logo);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setLogo(R.drawable.logo);
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        // Set color select mode
+        colorMode = false;
 
         // Attach views to variables
         mImageView = (ImageView) findViewById(R.id.captured_image_thumbnail);
+        mHighColorPatchView = (ImageView) findViewById(R.id.high_color_patch_view);
+        mLowColorPatchView = (ImageView) findViewById(R.id.low_color_patch_view);
+        mDotModeImageButton = (ImageButton) findViewById(R.id.dot_mode_image_button);
+        mCameraButton = (Button) findViewById(R.id.capture_image_button);
+        mUploadButton = (Button) findViewById(R.id.upload_data_button);
+        mEditText = (EditText) findViewById(R.id.description_edit_text);
+        mUserEditText = (EditText) findViewById(R.id.user_edit_text);
+        mDebugText = (TextView) findViewById(R.id.debug_text_display);
 
-        mBlackDotView = new ImageView(this);//(ImageView) findViewById(R.id.black_dot_view);
+        // Dynamic black dot stuff
+        mBlackDotView = new ImageView(this);
         mBlackDotView.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT));
@@ -83,20 +100,44 @@ public class MainActivity extends AppCompatActivity {
         // Width and height
         mBlackDotView.getLayoutParams().width = 50;
         mBlackDotView.getLayoutParams().height = 50;
-        // Set ID
+        // Set ID, so we can refer to it later
         mBlackDotView.generateViewId();
         mBlackDotView.setId(R.id.black_dot_view);
 
-        //mWhiteDotView = (ImageView) findViewById(R.id.white_dot_view);
-        mCameraButton = (Button) findViewById(R.id.capture_image_button);
-        mUploadButton = (Button) findViewById(R.id.upload_data_button);
-        mEditText = (EditText) findViewById(R.id.description_edit_text);
-        mDebugText = (TextView) findViewById(R.id.debug_text_display);
+        // Same white dot stuff
+        mWhiteDotView = new ImageView(this);
+        mWhiteDotView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        mWhiteDotView.setImageResource(R.drawable.white_dot);
+        mWhiteDotView.getLayoutParams().width = 50;
+        mWhiteDotView.getLayoutParams().height = 50;
+        mWhiteDotView.generateViewId();
+        mWhiteDotView.setId(R.id.white_dot_view);
+
+        // Disable button at start
+        mUploadButton.setEnabled(false);
 
         // Create button event listeners
+        mDotModeImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Invert the modes
+                if (colorMode) {
+                    mDotModeImageButton.setImageResource(R.drawable.black_dot);
+                    colorMode = false;
+                } else {
+                    mDotModeImageButton.setImageResource(R.drawable.white_dot);
+                    colorMode = true;
+                }
+            }
+        });
         mCameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+                // Update UI controls
+                mCameraButton.setText("Recapture");
+               // mCameraButton.refreshDrawableState();
+                mUploadButton.setEnabled(true);
             }
         });
         mUploadButton.setOnClickListener(new View.OnClickListener() {
@@ -108,22 +149,95 @@ public class MainActivity extends AppCompatActivity {
         mImageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                // TODO: Don't let screen rotate
+                // TODO: Better thumbnail view
+                // TODO: Don't let dots outside of Bitmap area
+                // TODO: Fix black dot placement
+                // TODO: Fix this terrible inefficient, last-minute code
+
+                // NOTE: ImageView is set to wrap_content, so the width of ImageView is the
+                // width of the encapsulated BitMap object.
+
+                // Don't go any further, unless we've taken a picture
+                if (mCurrentPhotoPath == null) { return true; }
+
+                // Debug display
                 mDebugText.setText("Touch coordinates : " +
                         String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
 
+                // Debug, stuff about ImageView
+                //putPermCircle((int) event.getX(), (int) event.getY());
+                //putPermCircle((int) mImageView.getX(), (int) mImageView.getY());
+
                 // Add black dot to screen, if it's not already there
-                if (findViewById(mBlackDotView.getId()) == null) {
+                if (colorMode) {
+                    if (findViewById(mWhiteDotView.getId()) == null) {
+                        ContentFrameLayout parent = (ContentFrameLayout) findViewById(android.R.id.content);
+                        parent.addView(mWhiteDotView);
+                    }
+                } else {
+                    if (findViewById(mBlackDotView.getId()) == null) {
                         ContentFrameLayout parent = (ContentFrameLayout) findViewById(android.R.id.content);
                         parent.addView(mBlackDotView);
+                    }
                 }
 
-                // Setting image position
-                mBlackDotView.setX(event.getX());
-                mBlackDotView.setY(event.getY());
+                // Display mBlackDotView if within ImageView bounds
+                if (isInView(mImageView, (int) event.getX(), (int) event.getY())) {
+
+                    // Set color patch color, so we can see which color we've touched on ImageView
+                    int selectedPixel = getPixelAtPos((int) event.getX(), (int) event.getY());
+
+                    if (colorMode) {
+                        mWhiteDotView.setVisibility(View.VISIBLE);
+
+                        mLowColorPatchView.setBackgroundColor(Color.rgb(Color.red(selectedPixel), Color.green(selectedPixel), Color.blue(selectedPixel)));
+                        mWhiteDotView.setX(event.getX());
+                        mWhiteDotView.setY(event.getY());
+                    } else {
+                        mBlackDotView.setVisibility(View.VISIBLE);
+
+                        mHighColorPatchView.setBackgroundColor(Color.rgb(Color.red(selectedPixel), Color.green(selectedPixel), Color.blue(selectedPixel)));
+                        // Setting dot position
+                        mBlackDotView.setX(event.getX());
+                        mBlackDotView.setY(event.getY());
+                    }
+                // Otherwise, hide it
+                } else {
+                    if (colorMode) mWhiteDotView.setVisibility(View.INVISIBLE);
+                    else mBlackDotView.setVisibility(View.INVISIBLE);
+                }
 
                 return true;
             }
         });
+    }
+
+    // Mainly a debug function to put black circle where I see fit
+    public void putPermCircle(int x, int y) {
+        ImageView circle = new ImageView(this);
+        circle.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        // Setting image resource
+        circle.setImageResource(R.drawable.black_dot);
+        // Width and height
+        circle.getLayoutParams().width = 25;
+        circle.getLayoutParams().height = 25;
+
+        circle.setX(x);
+        circle.setY(y);
+
+        ContentFrameLayout parent = (ContentFrameLayout) findViewById(android.R.id.content);
+        parent.addView(circle);
+    }
+
+    // Let us know if we are within a view
+    public boolean isInView(View view, int x, int y) {
+        return (x > view.getX())
+                && (x <  (view.getX() + view.getWidth()))
+                && (y > view.getY())
+                && (y < (view.getY() + view.getHeight()));
     }
 
     // Gets pixel color inside mImageView given x and y coordinates
@@ -149,14 +263,19 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = new File(storageDir, imageFileName + ".jpg");
+
+        /*
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,  // prefix
+                ".jpg",         // suffix
+                storageDir      // directory
         );
+        */
 
         // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        mCurrentPhotoPath = image.getAbsolutePath();//"file:" + image.getAbsolutePath();
         return image;
     }
 
@@ -181,13 +300,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Resize the given Bitmap
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+
+        return resizedBitmap;
+    }
+
     // Receives taken picture as thumbnail
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+
+            // Scale up the image size
+            int scaleUp = 4;
+            Bitmap resizedImageBitmap = getResizedBitmap(imageBitmap, imageBitmap.getWidth() * scaleUp, imageBitmap.getHeight() * scaleUp);
+
+            mImageView.setImageBitmap(resizedImageBitmap);
 
             mDebugText.setText("Captured and saved image as '" + mCurrentPhotoPath + "'");
         }
@@ -197,6 +339,7 @@ public class MainActivity extends AppCompatActivity {
     private void uploadData()
     {
         //lets get our picture again because that's fun
+        /*
         Bitmap bitmap;
         if(mImageView.getDrawable() instanceof BitmapDrawable)
         {
@@ -207,16 +350,32 @@ public class MainActivity extends AppCompatActivity {
             Drawable d = mImageView.getDrawable();
             bitmap = Bitmap.createBitmap(d.getIntrinsicWidth(),d.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
         }
+        */
 
-        // now convert the bitmap into a string
+        Bitmap bitmap = null;
         String i = "";
-        if(bitmap != null) {
-            i = Base64.encodeToString(getBytesFromBitmap(bitmap), Base64.DEFAULT);
+        if (mCurrentPhotoPath != null)
+        {
+            // Convert full image to Bitmap to String
+            File f = new File(mCurrentPhotoPath);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            try {
+                bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            if(bitmap != null) {
+                i = Base64.encodeToString(getBytesFromBitmap(bitmap), Base64.DEFAULT);
+            }
         }
 
         // creates our async network manager and sends the data off to be packaged
         NetworkManager nwork = new NetworkManager();
-        nwork.execute(mServerURL, mUser,mEditText.getText().toString(), i);
+        // TODO: Update below line to make better
+        String user = mUserEditText.getText().toString();
+        nwork.execute(mServerURL, user, mEditText.getText().toString(), i);
 
     }
 
